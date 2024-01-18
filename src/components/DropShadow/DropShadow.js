80efb7748d2a8cbb3aa68a35cc39ee3dc0430c17
Aditya Sharma <adityaksharma@gmail.com>
2024-01-17 16:19:48 -0800

    Update fonts and lower luminance in DropShadow.js

--- src/components/DropShadow/DropShadow.js
+++ src/components/DropShadow/DropShadow.js
@@ -72,7 +72,7 @@ function DropShadow({ color, elevation, shadowStyle, layerAmount, opacity, blur,
   if (!parsedColor.isValid()) {
     parsedColor.set('black');
   }
-  parsedColor = lowerLuminance(parsedColor, 20);
+  parsedColor = lowerLuminance(parsedColor, 10);
   const hsl = parsedColor.toHsl();
   const state = {
     shadowStyle: shadowStyle || "soft",
