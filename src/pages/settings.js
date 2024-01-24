f24b13fdae6d4d5a74274c9730684f0a14fff818
Aditya Sharma <adityaksharma@gmail.com>
2024-01-23 23:32:46 -0800

    Update serviceWorker.js, FeedCard.css, ReaderView.css, settings.css, and settings.js

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   SlButton,
   SlInput,
@@ -13,7 +13,8 @@ function Settings({
   refreshInterval,
   setRefreshInterval,
 }) {
-  const [newFeedUrl, setNewFeedUrl] = React.useState("");
+  const [newFeedUrl, setNewFeedUrl] = useState("");
+  const [urlError, setUrlError] = useState("");
   const inputRef = useRef();
 
   useEffect(() => {
@@ -24,10 +25,22 @@ function Settings({
     }
   }, []);
 
+  const isValidUrl = (url) => {
+    const pattern = new RegExp('^(https?:\\/\\/)', 'i'); // Check if starts with http:// or https://
+    return pattern.test(url);
+  };
+
   const handleAddFeed = () => {
+    if (!newFeedUrl || !isValidUrl(newFeedUrl)) {
+      setUrlError("Invalid URL. It must start with http:// or https://");
+      return;
+    }
     setFeedUrls([...feedUrls, newFeedUrl]);
     setNewFeedUrl("");
+    setUrlError(""); // Clear any existing error
   };
+
+  
 
   const handleRemoveFeed = (url) => {
     setFeedUrls(feedUrls.filter((feedUrl) => feedUrl !== url));
@@ -37,18 +50,18 @@ function Settings({
     <div className="settings">
       <h2>Settings</h2>
       <div className="subscription-form">
-        
         <SlInput
           ref={inputRef}
           type="text"
           value={newFeedUrl}
-          placeholder="New feed URL"
+          placeholder="http://feed.url/rss.xml"
           clearable
         >
           <SlIcon library="iconoir" name="rss-feed" slot="prefix"></SlIcon>
         </SlInput>
         <SlButton onClick={handleAddFeed}>Add Feed</SlButton>
       </div>
+      {urlError && <p className="error-message">{urlError}</p>}
       <div id="subscribed-feeds-list">
         {feedDetails.map((detail, index) => (
           <div key={feedUrls[index]} className="list-item">
