d6fcb6d8f414bd90f6a8ae71a87e0e0d271b4b21
bumpyclock <adityaksharma@gmail.com>
2024-03-24 11:27:34 -0700

    Refactor Modal component to handle animation and click outside behavior

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,35 +1,37 @@
-// Modal.js
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SlAnimation from "@shoelace-style/shoelace/dist/react/animation";
 import "./Modal.css"; // Your CSS file for Modal styling
 
 const Modal = ({ children, visible, onClose }) => {
   const modalRef = useRef(null);
   const childRef = useRef(null);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-  useEffect(
-    () => {
-      function handleClickOutside(event) {
-        if (modalRef.current && !modalRef.current.contains(event.target)) {
-          onClose();
-        }
+  useEffect(() => {
+    setIsAnimating(visible);
+  }, [visible]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (childRef.current && !childRef.current.contains(event.target)) {
+        setIsAnimating(false);
+        setTimeout(onClose, 500); // Delay onClose until the animation finishes
       }
+    }
 
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-        // Unbind the event listener on clean up
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    },
-    [onClose]
-  );
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
 
   return (
-    <SlAnimation name="fade-in" duration={500} play={children !== null}>
+    <SlAnimation name={visible ? "fade-in" : "fade-out"} duration={125} play={isAnimating}>
       <div className="modal-container visible" ref={modalRef}>
         <div className="modal-container-content" ref={childRef}>
-          {" "}{children}
+          {children}
         </div>
       </div>
     </SlAnimation>
