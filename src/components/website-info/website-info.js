48925de2cb0628f15dafe3b037ee4b3e731b5437
bumpyclock <adityaksharma@gmail.com>
2024-03-27 07:01:30 -0700

    Update CSS for FeedCard and website-info components

--- src/components/website-info/website-info.js
+++ src/components/website-info/website-info.js
@@ -4,7 +4,7 @@ import './website-info.css';
 const WebsiteInfo = ({ favicon, siteTitle, feedTitle, style }) => {
     return (
         <div className="website-info" style={style}>
-            <img src={favicon} alt={`${siteTitle} Favicon`} className="site-favicon" />
+            {favicon && <img src={favicon} alt={`${siteTitle} Favicon`} className="site-favicon" />}
             <p className='site-name'>{feedTitle || siteTitle}</p>
         </div>
     );
