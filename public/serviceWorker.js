c2245b878985cbad9e2dcefa88b493a7d592ff40
bumpyclock <adityaksharma@gmail.com>
2024-03-27 21:34:47 -0700

    Update CACHE_NAME in serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // Activate the new service worker and take control of the pages
-const CACHE_NAME = '24_03_27';
+const CACHE_NAME = '24_03_27_v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
