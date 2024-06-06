5d279e379dc69c2bae20a1f78371ecec2b98af89
Aditya Sharma <adityaksharma@gmail.com>
2024-06-06 15:16:00 -0700

    chore: Update serviceWorker.js cache name to '06_06_24_v1'

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // Activate the new service worker and take control of the pages
-const CACHE_NAME = '22_05_24_v1';
+const CACHE_NAME = '06_06_24_v1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
